76036ae6c54671bf92d18a3d9e93e6b48815ad6d
Bardo91 <19Bardo91@gmail.com>
2021-07-04 00:48:22 +0200

    Added first sample app with simple code WO dependencies but OS depent configuration

---
+++ apps/app1/src/main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+
+int main(){
+
+    std::cout << "I am app1. And I have learned to rap" << std::endl;
+    std::cout <<    "I said-a hip, hop, the hippie, the hippie\n"
+                    "To the hip hip hop-a you don't stop the rock\n"
+                    "It to the bang-bang boogie, say up jump the boogie\n"
+                    "To the rhythm of the boogie, the beat" << std::endl;
+}
