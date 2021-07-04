4fee4ac7a5e82b2974100fbd798b7cb5a3cff031
Bardo91 <19bardo91@gmail.com>
2021-07-04 02:59:51 +0200

    quick fix of resources opening in app3 for MacOS

--- apps/app3/src/main.cpp
+++ apps/app3/src/main.cpp
@@ -7,9 +7,17 @@
 int main(int _argc, char** _argv){
     std::filesystem::path p = _argv[0];
     auto parentPath = p.parent_path();
-    auto imgPath = parentPath/"Resources"/"sfdk.jpg";
-    std::cout << imgPath.string() << std::endl;
-    auto img = cv::imread(imgPath.string());
+    auto imgPath = (parentPath/"Resources"/"sfdk.jpg").string();
+
+    if(imgPath.find("MacOS") != imgPath.npos){
+        // Structure in MacOS is
+        // Content  / MacOS / app3
+        //          / Resources / sfdk.jpg
+        imgPath = (parentPath.parent_path()/"Resources"/"sfdk.jpg").string();
+    }
+
+    std::cout << imgPath << std::endl;
+    auto img = cv::imread(imgPath);
     if(img.rows >= 0){
         cv::imshow("Siempre Fuertes De Konziencia", img);
         cv::waitKey();
