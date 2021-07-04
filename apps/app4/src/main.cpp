24aeac39cfabb2c86367156691719a5be81782e8
Bardo91 <19bardo91@gmail.com>
2021-07-04 03:37:06 +0200

    added example with music play

--- apps/app4/src/main.cpp
+++ apps/app4/src/main.cpp
@@ -2,22 +2,44 @@
 #include <iostream>
 
 #include <QApplication>
+#include <filesystem>
 
-#include <QLabel>
+#include <QMediaPlayer>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QWidget>
 
 int main(int _argc, char** _argv){
     
     QApplication app(_argc, _argv);
 
-    QLabel label(   "YEAH, I am app 2 and I rap too!\n"
-                    "Yeah, uh huh, you know what it is (what it is) \n"
-                    "Everything I do, I do it big \n"
-                    "Yeah, uh huh, screamin' that's nothin' \n"
-                    "What I pulled off the lot, that's stuntin' \n"
-                    "Reppin' my town when you see me you know everything \n"
-                    "Black and yellow \n"
-                    "Black and yellow\n");
-    label.show();
+    std::filesystem::path p = _argv[0];
+    auto parentPath = p.parent_path();
+    std::string musicPath = (parentPath/"Resources"/"music.mp3").string();
+    if(musicPath.find("MacOS") != musicPath.npos){
+        musicPath = (parentPath.parent_path()/"Resources"/"music.mp3").string();
+    }
+
+    QMediaPlayer *player = new QMediaPlayer();
+    player->setVolume(50);
+    QString qpath = musicPath.c_str();
+    player->setMedia(QUrl::fromLocalFile(qpath));
+
+    QWidget *cw = new QWidget;
+    QPushButton *btPlay = new QPushButton("Play");
+    QObject::connect(btPlay, &QPushButton::clicked,[&](){ player->play(); });
+    QPushButton *btPause = new QPushButton("Pause");
+    QObject::connect(btPause, &QPushButton::clicked,[&](){ player->pause(); });
+    QPushButton *btStop = new QPushButton("Stop");
+    QObject::connect(btStop, &QPushButton::clicked,[&](){ player->stop(); });
+
+    QHBoxLayout *l = new QHBoxLayout;
+    cw->setLayout(l);
+    l->addWidget(btPlay);
+    l->addWidget(btPause);
+    l->addWidget(btStop);
+
+    cw->show();
 
     return app.exec();
 }
