a3d1228aa1c9baabf453ed324865c73e34b8944e
Bardo91 <19bardo91@gmail.com>
2021-07-04 14:34:34 +0200

    WIP automatic copy of plugins

--- cmake/qtHelpers.cmake
+++ cmake/qtHelpers.cmake
@@ -1,12 +1,31 @@
 
 
 
-function(getQtPluginList _qtLibName _pluginList)
-    message(STATUS "Analyzing plugins from ${_qtLibName}")
-    message(STATUS "Found: ${Qt5${_qtLibName}_PLUGINS}")
-    set(${_pluginList} "" PARENT_SCOPE)
+macro(getQtPluginList _qtLibName _pluginList)
     foreach(plugin ${Qt5${_qtLibName}_PLUGINS})
         list(APPEND ${_pluginList} ${plugin})
     endforeach()
-    message(STATUS "final list: ${${_pluginList}}")
-endfunction(getQtPluginList _qtLibName _pluginList)
+endmacro(getQtPluginList _qtLibName _pluginList)
+
+
+macro(getLocationTargets _targetList _targetsLocation)
+    foreach(target ${${_targetList}})
+        get_target_property(loc ${target} LOCATION)
+        list(APPEND ${_targetsLocation} ${loc})
+    endforeach()
+endmacro(getLocationTargets _targetList _targetsLocation)
+
+macro(copyPluginsToBinTarget _targetLocation _pluginsList _copiedPluginList)
+    set(pluginsLocation "")
+    getLocationTargets(${_pluginsList} pluginsLocation)
+    
+    foreach(plugin ${pluginsLocation})
+        get_filename_component(file ${plugin} NAME)
+        get_filename_component(loc ${plugin} DIRECTORY)
+        get_filename_component(pluginDir ${loc} NAME)
+
+        file(COPY ${plugin} DESTINATION ${_targetLocation}/${pluginDir})
+        
+        list(APPEND ${_copiedPluginList} ${_targetLocation}/${pluginDir}/${file})
+    endforeach()
+endmacro(copyPluginsToBinTarget _target _pluginsList )
